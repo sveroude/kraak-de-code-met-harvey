0d4ef5f42ec4dd041fcbc5d3e8577a455a3bdcad
Sanne Veroude <veroude.sanne@gmail.com>
2026-09-09 15:55:18 +0200

    replace with correct code

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ function checkNumber() {
   if (inputNumber === "") {
     resultElement.textContent = "Je hebt geen code ingevoerd!";
   } else {
-    const code = 2341;
+    const code = 3545;
     resultElement.textContent = `The number is ${result}.`;
     if (inputNumber == code) {
         resultElement.textContent = 'Gelukt! De code is goed!';
